072f0362e9df8c1362c0a116b92af7b97dfa16d6
alianora <nastya.den1104@gmail.com>
2021-03-02 17:59:41 +0200

    commit

---
+++ vue/hello-world/src/components/SlickSlider.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="slider">
+  <VueSlickCarousel v-bind="settings">
+    <div class="slider-item" v-for="item in items" :key="item.id"><h3>{{item.id}}</h3></div>
+  </VueSlickCarousel>
+  </div>
+</template>
+
+<script>
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+export default {
+name: "SlickSlider",
+  components: { VueSlickCarousel },
+  data:
+      function(){
+    return {
+      settings: {
+        "dots": false,
+        "arrows": true,
+        "edgeFriction": 0.35,
+        "infinite": true,
+        "speed": 500,
+        "slidesToShow": this.quantity,
+        "slidesToScroll": this.quantity,
+        "touchThreshold": 5
+      }
+    }
+  },
+props: ['items','quantity']
+
+}
+</script>
+
+<style scoped>
+.slick-prev, .slick-next {
+  background-color: #2c3e50;
+}
+.slider {
+  padding: 20px 50px;
+  margin: 10px;
+  background-color: #2c3e50;
+}
+.slider-item {
+  background-color: antiquewhite;
+}
+</style>
---
+++ vue/hello-world/src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="list">
+     <div class="item" v-for="item in list" :key="item.value">
+       {{item.value}}{{item.name}}
+     </div>
+    <transition name="opacity">
+      <div class="kss" v-if="customItem"> lksdlksdlksd</div>
+    </transition>
+
+    <button @click="something">click <span v-if="count">{{count}}</span></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data: function(){
+    return {
+      list: this.test,
+      customItem: false,
+      count: 0
+    }
+  },
+  props: ['test','ttt'],
+  methods: {
+    something: function (){
+      this.customItem = !this.customItem;
+    }
+  },
+  updated() {
+    console.log(this);
+    if (this.list.length > 2 && this.count === 0){
+      this.count = this.count + this.ttt;
+    }
+  }
+}
+</script>
+
+<style>
+  .opacity-enter-active, .opacity-leave-active {
+    transition: all .5s;
+  }
+  .opacity-enter, .opacity-leave-to {
+    transform: scale(0);
+  }
+</style>
